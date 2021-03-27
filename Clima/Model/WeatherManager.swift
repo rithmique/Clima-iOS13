a598c5a81c84f09a6ff45f06b9552a71ad59c0d0
Maxim Vainikka <rithmique@yahoo.com>
2021-03-28 00:23:26 +0100

    Update WeatherManager.swift

--- Clima/Model/WeatherManager.swift
+++ Clima/Model/WeatherManager.swift
@@ -10,9 +10,31 @@ import UIKit
 
 struct WeatherManager {
     let city: String = ""
-    let weatherURL = "http://api.openweathermap.org/data/2.5/find?appid=0c7121f670162ed550582cc9e699da68&units=metric"
+    let weatherURL = "https://api.openweathermap.org/data/2.5/find?appid=put here ur id&units=metric"
     
     func getWeather(cityName: String) {
-        let URLString = "\(weatherURL)&q=\(cityName)"
+        let urlString = "\(weatherURL)&q=\(cityName)"
+        performRequest(urlString: urlString)
+    }
+    func performRequest(urlString: String) {
+        
+        //create an url
+        if let url = URL(string: urlString) {
+            //create an url session
+            let urlSession = URLSession(configuration: .default)
+            //provide the url session with a task
+            let task = urlSession.dataTask(with: url, completionHandler: handle(data: urlResponse: error:))
+            //start the task
+            task.resume()
+        }
+    }
+    func handle(data: Data?, urlResponse: URLResponse?, error: Error?) {
+        if error != nil {
+            print(error!)
+            return
+        }
+        if let safedata = data {
+            let dataString = String(data: safedata, encoding: .utf8)
+        }
     }
 }
